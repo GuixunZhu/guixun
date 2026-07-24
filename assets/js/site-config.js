e5fcf035e3b050061f1246cdad7d97dbef3a3535
GuixunZhu <151450404+GuixunZhu@users.noreply.github.com>
2026-07-24 19:05:14 +0800

    Update site-config.js

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -3,11 +3,11 @@
  * Leave a field empty to hide the corresponding link on the website.
  */
 window.GZ_SITE_CONFIG = {
-  email: "",
-  githubUsername: "",
-  googleScholarUrl: "",
-  orcidUrl: "",
-  researchGateUrl: "",
+  email: "gxzhu@dlut.edu.cn",
+  githubUsername: "https://github.com/GuixunZhu",
+  googleScholarUrl: "https://scholar.google.com/citations?user=CQUnhpAAAAAJ&hl=zh-CN&oi=sra",
+  orcidUrl: "https://orcid.org/0000-0002-9900-7565",
+  researchGateUrl: "https://www.researchgate.net/profile/Guixun-Zhu?ev=hdr_xprf",
   affiliationZh: "大连理工大学船舶工程学院",
   affiliationEn: "School of Naval Architecture and Ocean Engineering, Dalian University of Technology"
 };
