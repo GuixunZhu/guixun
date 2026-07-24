d92a1ed59922102aa1f8fc14a1eda99241f8bfd6
GuixunZhu <151450404+GuixunZhu@users.noreply.github.com>
2026-07-24 19:09:25 +0800

    Fix GitHub username URL format

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -4,7 +4,7 @@
  */
 window.GZ_SITE_CONFIG = {
   email: "gxzhu@dlut.edu.cn",
-  githubUsername: "https://github.com/GuixunZhu",
+  githubUsername: "https://github.com/GuixunZhu/",
   googleScholarUrl: "https://scholar.google.com/citations?user=CQUnhpAAAAAJ&hl=zh-CN&oi=sra",
   orcidUrl: "https://orcid.org/0000-0002-9900-7565",
   researchGateUrl: "https://www.researchgate.net/profile/Guixun-Zhu?ev=hdr_xprf",
